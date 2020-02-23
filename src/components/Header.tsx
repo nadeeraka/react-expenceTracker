c43a2233a782a8f7c1a6eaebff7f5165ef8f3697
nadeeraka <nadeerakan@gmail.com>
2020-02-23 19:22:25 +0530

    refactore

---
+++ src/components/Header.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Typography from "@material-ui/core/Typography";
+
+export const Header: React.FC = () => {
+  return (
+    <div>
+      <Typography variant="h2" gutterBottom>
+        Expencyfy
+      </Typography>
+    </div>
+  );
+};
